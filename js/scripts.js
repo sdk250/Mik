daab69d7a9d277b6b6ab00eb3b72b72aa4899fcb
sdk250 <520sdk250@gmail.com>
2022-05-18 18:49:38 +0800

    Update version to 2.4.

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ this.playCode = 1;
 audio.controls = true;
 this.state = 0;
 this.playerState = true;
-dark.selectId("test").innerText = 2.3;
+dark.selectId("test").innerText = 2.4;
 
 var play = dark.selectId("play");
 var pause = dark.selectId("pause");
